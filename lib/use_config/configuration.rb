514a27afd986f676050a9a4e697d9f1d105e4f06
Svetoslav Chernobay <slava@chernobay.info>
2011-03-05 00:51:19 +0300

    Documented UseConfig::Configuration.

--- lib/use_config/configuration.rb
+++ lib/use_config/configuration.rb
@@ -2,11 +2,33 @@ require 'rubygems'
 require 'yaml'
 require 'thread'
 
-gem 'hash-access'
+require 'hash_access'
 
 module UseConfig
   include HashAccess
 
+  # UseConfig class plays two roles.
+  #
+  # An instance of the class is a hash, a placeholder for configuration.
+  # It contents could be loaded from a YAML file or could be populated
+  # manually. Configuration values can be accessed using the hash notation
+  # +object['key']+ as well as using the method notation +object.key+.
+  #
+  # The class itself is a container. It containas above-mentioned configuration
+  # objects that are stored in the +conf+ hash attribute which is accessible
+  # by the +conf+ method.
+  #
+  # A particular instance should be created with the +add_conf+ method.
+  # It is not intended to use as an instance as a standalone object.
+  #
+  # Usage example:
+  #
+  #     UseConfig::Configuration.add_conf self, :qwerty, :empty => true
+  #     UseConfig::Configuration.conf.qwerty.param_1 = 'Value 1'
+  #     print UseConfig::Configuration.conf.qwerty.to_s, "\n"
+  #
+  # This class is designed as the UseConfig supplemental class. It is not very
+  # useful by its own.
   class Configuration < Hash
     class << self
       attr_accessor :accessors
@@ -30,6 +52,27 @@ module UseConfig
       attr_accessor a
     end
 
+    # Creates a new config (an instance of +self+) (calls +self.new+)
+    # and inserts the result object it into +self.conf+ hash as
+    # +self.conf[name]+ element. If the config already exists and the
+    # +:reload_when_add+ class setting is set - reloads config contents.
+    #
+    # Returns the created config object.
+    #
+    # Parameters
+    #
+    # * +used_by+ - The class that uses the config. After the config created
+    #   created, this class is assotiated with it. This prevents the config to
+    #   be deleted if any class uses it.
+    # * +name+ - Config name. It is used as the +conf+ hash key.
+    # * +options+ - The options hash to be passed to +new+.
+    #
+    # Options
+    #
+    # * +:empty+ - Create an empty config. Don't use a YAML file to load data.
+    # * +:file+ - Use the mentioned file instead of the default.
+    # * +:path_insert+ - Add this value to the beginning of the configuration
+    #   files' search path.
     def self.add_conf(used_by, name, options = {}, &block)
       sname = name.to_s
       self.mutex.synchronize do
@@ -38,13 +81,14 @@ module UseConfig
           self.conf[sname].used_by_add(used_by)
           yield self.conf[sname] if block_given?
         else
-          self.conf[sname] = UseConfig::Configuration.new(sname, options, &block)
+          self.conf[sname] = self.new(sname, options, &block)
           self.conf[sname].used_by_add(used_by)
         end
         self.conf[sname]
       end # self.mutex.synchronize do
     end
 
+    # Deletes the config if it is not in use.
     def self.drop_conf(used_by, name)
       sname = name.to_s
       self.mutex.synchronize do
@@ -92,16 +136,18 @@ module UseConfig
         end
         load_configuration
       end
-      access_by_methods
+      self.access_by_methods
       yield self if block_given?
       self
-    end # def initialize(name, options = {}, &block)
+    end # def initialize
 
+    # Adds the class to usage list.
     def used_by_add(used_by)
       self.used_by.push(used_by) unless self.used_by.include?(used_by)
       self
     end
 
+    # Removes the class from usage list.
     def used_by_drop(used_by)
       if self.used_by.include?(used_by)
         self.used_by.delete(used_by)
@@ -109,6 +155,7 @@ module UseConfig
       self
     end
 
+    # Serches for configuration file.
     def config_file_find
       self.config_path.each do |dir|
         self.extentions.each do |ext|
@@ -122,6 +169,7 @@ module UseConfig
       self
     end # def config_file_find
 
+    # Loads configuration from file +config_file+.
     def load_configuration
       cfg = YAML.load_file(config_file)
       cfg.each do |key, value|
@@ -131,6 +179,7 @@ module UseConfig
       self
     end
 
+    # Removes all the keys from itself.
     def clear!
       each_key do |key|
         next if self.class.accessors.include?(key.to_sym)
@@ -140,6 +189,7 @@ module UseConfig
       self
     end
 
+    # Reloads configuration from file.
     def reload!
       clear!
       load_configuration if use_config_file
